7fb3a96e59734a6fd61495203961630b94f5c53a
flint <olusegun.sola19@gmail.com>
2019-11-11 13:36:17 +0100

    Updates

---
+++ precache-manifest.02762549ca337df4327ce5ae77812f59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1849e13b1c3ebd8ef13cc47ef2534f",
+    "url": "/haywhy2019/foodquiz.git/index.html"
+  },
+  {
+    "revision": "d9aa162db11a499668e4",
+    "url": "/haywhy2019/foodquiz.git/static/css/main.191fa204.chunk.css"
+  },
+  {
+    "revision": "b21b56089c44434ba300",
+    "url": "/haywhy2019/foodquiz.git/static/js/2.5832ccd0.chunk.js"
+  },
+  {
+    "revision": "d9aa162db11a499668e4",
+    "url": "/haywhy2019/foodquiz.git/static/js/main.23d09917.chunk.js"
+  },
+  {
+    "revision": "a544bac2ad28897a674e",
+    "url": "/haywhy2019/foodquiz.git/static/js/runtime-main.bcd6cfa3.js"
+  }
+]);
